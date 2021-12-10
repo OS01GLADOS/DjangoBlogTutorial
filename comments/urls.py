692adc91159f47d85058852b7a3679e14535ca6b
Teiko <no.name.tyan.d@gmail.com>
2021-12-10 11:26:31 +0300

    now comments works properly

--- comments/urls.py
+++ comments/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:post_id>/add_comment', views.add_comment, name='comments-add')
+    path('<int:post_id>/add_comment', views.add_comment, name='comments-add'),
+    path('<int:post_id>/get_comments',views.get_comments, name='comments-get'),
 ]
--- comments/views.py
+++ comments/views.py
@@ -1,7 +1,18 @@
-from django.http import HttpResponse
+import json
+from datetime import time
+
+from django.conf.urls import url
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
+from django.utils import timezone
+
+from blog.models import Post
 from comments.forms import CommentCreateForm
 from django.contrib import messages
-from . models import Comments
+
+from users.models import Profile
+from .models import Comments
 
 
 # Create your views here.
@@ -9,8 +20,28 @@ def add_comment(request, post_id):
     form = CommentCreateForm(request.POST or None)
     if form.is_valid():
         comment = Comments(content=form.data.get('content'),
-                           post_id=post_id,
+                           post_id=get_object_or_404(Post, pk=post_id).id,
                            sender_id=request.user.id)
         comment.save()
         messages.success(request=request, message='comment was added')
-        return HttpResponse('added successfully')
+    return redirect('post-detail', post_id)
+
+
+def get_comments(request, post_id):
+    date = request.GET.get('time')
+    res = []
+    if date != 0:
+        res = list(
+            Comments.objects.filter(post_id=post_id).filter(date_posted__gte=date).values('content',
+                                                            'sender__username',
+                                                            'date_posted',
+                                                            'sender__id'
+                                                            )
+        )
+    for elem in res:
+        elem['date_posted'] = elem['date_posted'].strftime(
+            '%H:%M %b %d, %Y')
+        elem['sender_link'] = reverse('user-posts', kwargs={'username': elem['sender__username']} ),
+        elem['sender_pic'] = Profile.objects.get(id=elem['sender__id']).image.url
+    response = JsonResponse(json.dumps(res), safe=False)
+    return response
