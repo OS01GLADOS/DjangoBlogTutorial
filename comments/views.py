8939f529b40600c4a7f2842a5844e08dc7971731
OS01GLADOS <no.name.tyan.d@gmail.com>
2022-02-06 17:04:53 +0300

    db changed to postgress, project changed to pipenv

--- comments/views.py
+++ comments/views.py
@@ -1,21 +1,14 @@
 import json
-from datetime import time
-
-from django.conf.urls import url
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
-from django.utils import timezone
-
 from blog.models import Post
 from comments.forms import CommentCreateForm
 from django.contrib import messages
-
 from users.models import Profile
 from .models import Comments
 
 
-# Create your views here.
 def add_comment(request, post_id):
     form = CommentCreateForm(request.POST or None)
     if form.is_valid():
@@ -33,15 +26,15 @@ def get_comments(request, post_id):
     if date != 0:
         res = list(
             Comments.objects.filter(post_id=post_id).filter(date_posted__gte=date).values('content',
-                                                            'sender__username',
-                                                            'date_posted',
-                                                            'sender__id'
-                                                            )
+                                                                                          'sender__username',
+                                                                                          'date_posted',
+                                                                                          'sender__id'
+                                                                                          )
         )
     for elem in res:
         elem['date_posted'] = elem['date_posted'].strftime(
             '%H:%M %b %d, %Y')
-        elem['sender_link'] = reverse('user-posts', kwargs={'username': elem['sender__username']} ),
+        elem['sender_link'] = reverse('user-posts', kwargs={'username': elem['sender__username']}),
         elem['sender_pic'] = Profile.objects.get(id=elem['sender__id']).image.url
     response = JsonResponse(json.dumps(res), safe=False)
     return response
